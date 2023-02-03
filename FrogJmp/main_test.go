0a053fc03ff3c0545ab4255ae0fda007f225850c
Voprzybyo <wo.przybyo@gmail.com>
2023-02-03 20:19:53 +0100

    Correct code formatting

--- FrogJmp/main_test.go
+++ FrogJmp/main_test.go
@@ -1,28 +1,27 @@
 package main
 
 import "testing"
+
 func TestMinimalNumberOfJumps(t *testing.T) {
 
 	type test struct {
-		data1   int
-		data2   int
-		data3   int
+		data1  int
+		data2  int
+		data3  int
 		answer int
 	}
 
-tests := []test {
-	{10, 85, 30, 3},
-	{10, 20, 5, 2},
-	{5, 6, 1, 1},
-	{5,6, 80, 1},
-}
+	tests := []test{
+		{10, 85, 30, 3},
+		{10, 20, 5, 2},
+		{5, 6, 1, 1},
+		{5, 6, 80, 1},
+	}
 
 	for _, v := range tests {
 		x := MinimalNumberOfJumps(v.data1, v.data2, v.data3)
-		if(x != v.answer) {
+		if x != v.answer {
 			t.Error("Got: ", x, "Expected: ", v.answer)
 		}
 	}
 }
-
-
